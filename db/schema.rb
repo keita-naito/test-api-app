40d09e7810758a0cecffcb80e697ee615ebc4973
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-18 03:31:20 +0900

    記事に関するファイルに抜けがあったので追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_175959) do
+ActiveRecord::Schema.define(version: 2022_02_17_183039) do
+
+  create_table "articles", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
