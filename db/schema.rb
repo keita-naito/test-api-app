78d61d96765db14248f0314487a2249c7a60ffce
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-19 18:43:32 +0900

    記事に外部キーが抜けていたので追加

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2022_02_17_183039) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 2022_02_17_183039) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
