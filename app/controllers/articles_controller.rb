c5f1f812c565b266c12f1069ccb077394d30799f
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-02-19 18:50:37 +0900

    記事の一覧を返すようにレスポンスを調整

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     # ヘッダー情報を載せて送信できるようになったら再度調整する
-    current_user.articles
+    articles = current_user.articles
+    render json: articles, each_serializer: ArticlesSerializer
   end
 end
