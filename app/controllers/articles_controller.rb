f088004b3bb694120d315d231783931087f0fc10
keita-naito <atiek_nn_xx@yahoo.co.jp>
2022-05-24 00:43:25 +0900

    ログインなしでも記事を取得できるように調整

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     # ヘッダー情報を載せて送信できるようになったら再度調整する
-    articles = current_user.articles
+    articles = Article.all
     render json: articles, each_serializer: ArticlesSerializer
   end
 end
